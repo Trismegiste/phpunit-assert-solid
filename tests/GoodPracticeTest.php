43784e85110339839f86a0166c8f50235edf62e6
Trismegiste <flo@trismegiste.net>
2014-12-02 17:53:03 +0100

    tests the trait

---
+++ tests/GoodPracticeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * phpunit-solid-assertion
+ */
+
+namespace tests\Trismegiste\SolidAssert;
+
+/**
+ * GoodPracticeTest tests the GoodPractice trait
+ */
+class GoodPracticeTest extends \PHPUnit_Framework_TestCase
+{
+
+    use \Trismegiste\SolidAssert\GoodPractice;
+
+    public function testConcreteTypeHint()
+    {
+        $this->assertInterfaceHintedParameter('BadProject\Good1');
+    }
+
+    public function testContractForMethod()
+    {
+        $this->assertNoMethodWithoutContract('BadProject\Good2');
+    }
+
+    public function testSmallApi()
+    {
+        $this->assertSmallApi('BadProject\Good3');
+    }
+
+    public function testHollywoodPrinciple()
+    {
+        $this->assertHollywoodPrinciple('BadProject\Good4');
+    }
+
+    public function testLiskovCompliance()
+    {
+        $this->assertLiskovCompliant('BadProject\Good5');
+    }
+
+    public function testNotStaticFactory()
+    {
+        $this->assertNotStaticFactory('BadProject\Case4');
+    }
+
+}
